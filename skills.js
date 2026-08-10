371e372f71d63e27c5bd2746679cbc4c7515aa70
S31ZUR3 <saiprapanch2006@gmail.com>
2026-08-10 16:35:44 +0530

    update skill ratings, remove Go/Solidity/Blockchain, and fix background lag via canvas

--- skills.js
+++ skills.js
@@ -1,1 +1,1 @@
-const SKILLS = [["categories", ["Reverse Engineering", "Binary Exploitation", "Web Security", "Cryptography", "Blockchain", "Forensics"]], ["languages", ["Python", "C / C++", "Assembly (x64)", "JavaScript", "Go", "Solidity"]], ["tools", ["Ghidra", "GDB", "Burp Suite", "Docker", "Wireshark", "pwntools"]]]
+const SKILLS = [["categories", ["Reverse Engineering", "Binary Exploitation", "Web Security", "Cryptography", "Forensics"]], ["languages", ["Python", "C / C++", "Assembly (x64)", "JavaScript"]], ["tools", ["Ghidra", "GDB", "Burp Suite", "Docker", "Wireshark", "pwntools"]]]
